56657600c165732b1f41d5f69198f377c2c92cd0
andrut <dodiedogg@poczta.onet.pl>
2018-11-25 13:40:02 +0100

    dodane wypisanie liczb od 10 do 0 co 2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Git");
+        int liczba = 10;
+        System.out.println(liczba);
+        for (int i = 0; i < 5; i++) {
+            liczba -=2;
+
+            System.out.println(liczba);
+        }
     }
 }
