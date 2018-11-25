e8e318b6e25494b4567bfdcbfb4458cfc7e72120
andrut <dodiedogg@poczta.onet.pl>
2018-11-25 13:51:49 +0100

    poprawiona pętla for

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Git");
-        int liczba = 10;
-        System.out.println(liczba);
-        for (int i = 0; i < 5; i++) {
-            liczba -=2;
 
-            System.out.println(liczba);
+        for (int i = 10; i >= 0; i--) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+
         }
     }
 }
