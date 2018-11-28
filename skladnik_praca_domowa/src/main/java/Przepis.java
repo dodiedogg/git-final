85523afa7589bfad16d9d67f99738faaa3d7ee62
andrut <dodiedogg@poczta.onet.pl>
2018-11-28 23:17:22 +0100

    Dodane gettery i construktor

--- skladnik_praca_domowa/src/main/java/Przepis.java
+++ skladnik_praca_domowa/src/main/java/Przepis.java
@@ -7,4 +7,37 @@ public class Przepis {
     private int czasWykonaniaDania;
     private List<KrokPrzepisu> krokPrzepisuList;
     private List<Skladnik> listaSkladnikow;
+
+    public Przepis(String nazwaPrzepisu, PoraRoku poraRoku, String trescPrzepisu, int czasWykonaniaDania, List<KrokPrzepisu> krokPrzepisuList, List<Skladnik> listaSkladnikow) {
+        this.nazwaPrzepisu = nazwaPrzepisu;
+        this.poraRoku = poraRoku;
+        this.trescPrzepisu = trescPrzepisu;
+        this.czasWykonaniaDania = czasWykonaniaDania;
+        this.krokPrzepisuList = krokPrzepisuList;
+        this.listaSkladnikow = listaSkladnikow;
+    }
+
+    public String getNazwaPrzepisu() {
+        return nazwaPrzepisu;
+    }
+
+    public PoraRoku getPoraRoku() {
+        return poraRoku;
+    }
+
+    public String getTrescPrzepisu() {
+        return trescPrzepisu;
+    }
+
+    public int getCzasWykonaniaDania() {
+        return czasWykonaniaDania;
+    }
+
+    public List<KrokPrzepisu> getKrokPrzepisuList() {
+        return krokPrzepisuList;
+    }
+
+    public List<Skladnik> getListaSkladnikow() {
+        return listaSkladnikow;
+    }
 }
